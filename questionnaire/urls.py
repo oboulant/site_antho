c8b9ffef4f2cd06f17e31e82598fc52abf655d2e
Olivier Boulant <olivier.boulant@cmla.ens-cachan.fr>
2017-08-30 19:40:01 +0200

    Typo in urls AGAIN

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     url(r'^questionnaire/(?P<pk>[0-9]+)/$', views.questionnaire_detail, name = 'questionnaire_detail'),
     url(r'^example_chute_d_g/', views.example_chute_d_g, name = 'example_chute_d_g'),
     url(r'^example_chute_nd_ng_h/', views.example_chute_nd_ng_h, name = 'example_chute_nd_ng_h'),
-    rl(r'^example_chute_nd_ng_h1/', views.example_chute_nd_ng_h1, name = 'example_chute_nd_ng_h1'),
-    rl(r'^example_chute_nd_ng_h2/', views.example_chute_nd_ng_h2, name = 'example_chute_nd_ng_h2'),
-    rl(r'^example_chute_nd_ng_2h/', views.example_chute_nd_ng_2h, name = 'example_chute_nd_ng_2h'),
-    rl(r'^example_chute_nd_g/', views.example_chute_nd_g, name = 'example_chute_nd_g'),
+    url(r'^example_chute_nd_ng_h1/', views.example_chute_nd_ng_h1, name = 'example_chute_nd_ng_h1'),
+    url(r'^example_chute_nd_ng_h2/', views.example_chute_nd_ng_h2, name = 'example_chute_nd_ng_h2'),
+    url(r'^example_chute_nd_ng_2h/', views.example_chute_nd_ng_2h, name = 'example_chute_nd_ng_2h'),
+    url(r'^example_chute_nd_g/', views.example_chute_nd_g, name = 'example_chute_nd_g'),
 ]
