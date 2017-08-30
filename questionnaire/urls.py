01fa05720ce815e62172fb4531d210cd4e15b3b3
Olivier Boulant <olivier.boulant@cmla.ens-cachan.fr>
2017-08-30 19:38:39 +0200

    Typo in urls

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -11,7 +11,7 @@ from questionnaire import views
 urlpatterns = [
     url(r'^$', views.questionnaire_list, name = 'questionnaire_list'),
     url(r'^questionnaire/(?P<pk>[0-9]+)/$', views.questionnaire_detail, name = 'questionnaire_detail'),
-    url(r'^example_chute_d/', views.example_chute_d, name = 'example_chute_d'),
+    url(r'^example_chute_d_g/', views.example_chute_d_g, name = 'example_chute_d_g'),
     url(r'^example_chute_nd_ng_h/', views.example_chute_nd_ng_h, name = 'example_chute_nd_ng_h'),
     rl(r'^example_chute_nd_ng_h1/', views.example_chute_nd_ng_h1, name = 'example_chute_nd_ng_h1'),
     rl(r'^example_chute_nd_ng_h2/', views.example_chute_nd_ng_h2, name = 'example_chute_nd_ng_h2'),
